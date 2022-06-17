b1b2b41a4386b9f26550e5c1198a50de88f9930e
Alan Flores <alanif@hotmail.com>
2022-06-17 00:47:39 -0500

    refactor: erasing onClick functions on the buttons and adding eventListeners

--- cursoPracticoJavascript/Figuras/calcFig.js
+++ cursoPracticoJavascript/Figuras/calcFig.js
@@ -4,5 +4,21 @@ const calcularPCuadrado = (lado) =>{
 const calcularACuadrado = (lado) =>{
     return lado * lado;
 }
-
-export {calcularPCuadrado, calcularACuadrado};
+const calcularATriangulo = (base, altura) =>{
+    return (base * altura) / 2;
+}
+const calcularPTriangulo = (ladoA, ladoB, base) =>{
+    if(ladoA == ladoB && ladoB == ladoA && base == ladoA){
+        return `El <strong>perimetro</strong> del triangulo equilatero es de:<strong> ${ladoA*3}cm</strong>`;
+    }if(ladoA == ladoB && ladoA != base){
+        return `El <strong>perimetro</strong> del triangulo isosceles es de: <strong>${(ladoA * 2) + base}cm</strong>`;
+    }
+    return  `El <strong>perimetro</strong> del triangulo escaleno es de: <strong>${ladoA + ladoB + base}cm</strong>`
+}
+const calcularPCircle = (radio) =>{
+    return radio * 2 * Math.PI;
+}
+const calcularACircle = (radio) =>{
+    return Math.pow(radio, 2) * Math.PI;
+}
+export {calcularPCuadrado, calcularACuadrado, calcularATriangulo, calcularPTriangulo, calcularACircle, calcularPCircle};
--- cursoPracticoJavascript/Figuras/figuras.js
+++ cursoPracticoJavascript/Figuras/figuras.js
@@ -1,62 +1,78 @@
-import {calcularPCuadrado as perimetroC, calcularACuadrado as areaC} from './calcFig.js';
+import {
+  calcularPCuadrado as perimetroC,
+  calcularACuadrado as areaC,
+  calcularATriangulo as areaT,
+  calcularPTriangulo as perimetroT,
+  calcularACircle as areaCi,
+  calcularPCircle as perimetroCi
+} from "./calcFig.js";
 
 const btnPerimetroC = document.querySelector(".pC");
 const btnAreaC = document.querySelector(".aC");
-const answer = document.querySelector('.main__Cuadrado-form-answer');
+const btnAreaT = document.querySelector(".aT");
+const btnPerimetroT = document.querySelector(".pT");
+const btnPerimetroCi = document.querySelector(".pCi");
+const btnAreaCi = document.querySelector(".aCi");
+const answerC = document.querySelector(".main__Cuadrado-form-answer");
+const answerT = document.querySelector(".main__Triangle-form-answer");
+const answerCi = document.querySelector(".main__Circle-form-answer");
 
-const calcularPerimetroCuadrado = () =>{
-    const ladoCuadrado = Number(document.querySelector('.main__Cuadrado-form-input').value);
-    if(!ladoCuadrado){
-        return answer.innerHTML = `Favor de ingresar un dato numerico🧐`;
-    }
-    answer.innerHTML = `El <strong>perimetro</strong> del cuadrado es de: <strong>${perimetroC(ladoCuadrado)}cm</strong>`;
-}
+
+const calcularPerimetroCuadrado = () => {
+  const ladoCuadrado = Number(document.querySelector(".main__Cuadrado-form-input").value);
+  if (!ladoCuadrado) {
+    return (answerC.innerHTML = `Favor de ingresar un dato numerico🧐`);
+  }
+  answerC.innerHTML = `El <strong>perimetro</strong> del cuadrado es de: <strong>${perimetroC(ladoCuadrado)}cm</strong>`;
+};
 btnPerimetroC.addEventListener("click", calcularPerimetroCuadrado);
 
-const calcularAreaCuadrado = () =>{
-    const ladoCuadrado = Number(document.querySelector('.main__Cuadrado-form-input').value);
-    if(!ladoCuadrado){
-        return answer.innerHTML = `Favor de ingresar un dato numerico🧐` ;
-    }
-    answer.innerHTML = `El <strong>area</strong> del cuadrado es de: <strong>${areaC(ladoCuadrado)}cm<sup>2</sup></strong>`;
-}
+const calcularAreaCuadrado = () => {
+  const ladoCuadrado = Number(document.querySelector(".main__Cuadrado-form-input").value);
+  if (!ladoCuadrado) {
+    return (answerC.innerHTML = `Favor de ingresar un dato numerico🧐`);
+  }
+  answerC.innerHTML = `El <strong>area</strong> del cuadrado es de: <strong>${areaC(ladoCuadrado)}cm<sup>2</sup></strong>`;
+};
 btnAreaC.addEventListener("click", calcularAreaCuadrado);
 
+const calcularAreaTriangle = () => {
+  const medidas = document.querySelectorAll(".main__Triangle-form-input");
+  const base = Number(medidas[2].value);
+  const altura = Number(medidas[3].value);
+  if (!base || !altura) {
+    return (answerT.innerHTML = `Favor de ingresar un dato numericos🧐 en los campos necesarios.`);
+  }
+  answerT.innerHTML = `El <strong>area</strong> del triangulo es de: <strong>${areaT( base, altura)}cm<sup>2</sup></strong>`;
+};
+btnAreaT.addEventListener("click", calcularAreaTriangle);
 
-const calcularAreaTriangle = () =>{
-    const medidas = document.querySelectorAll(".main__Triangle-form-input");
-    if(!medidas[2].value || !medidas[3].value){
-        return answer.innerHTML = `Favor de ingresar un dato numericos🧐 en los campos necesarios.`;
-    }
-    answer.innerHTML = `El <strong>area</strong> del triangulo es de: <strong>${(medidas[2].value * medidas[3].value)/2}cm<sup>2</sup></strong>`
-}
-
-const calcularPerimetroTriangle= () =>{
-    const medidas = document.querySelectorAll(".main__Triangle-form-input");
-    if(!medidas[0].value ||!medidas[1].value || !medidas[2].value){
-        return answer.innerHTML = `Favor de ingresar un dato numericos🧐 en los campos necesarios.`;
-    }
-    if(medidas[0].value == medidas[1].value && medidas[0].value == medidas[2].value){
-        return answer.innerHTML = `El <strong>perimetro</strong> del triangulo equilatero es de:<strong> ${medidas[0].value*3}cm</strong>`;
-    }
-    if(medidas[0].value == medidas[1].value && medidas[0].value != medidas[2].value){
-        return answer.innerHTML = `El <strong>perimetro</strong> del triangulo isosceles es de: <strong>${parseInt((medidas[0].value * 2)) + parseInt(medidas[2].value)}cm</strong>`;
-    }
-    return answer.innerHTML = `El <strong>perimetro</strong> del triangulo escaleno es de: <strong>${parseInt(medidas[0].value) + parseInt(medidas[1].value) + parseInt(medidas[2].value)}cm</strong>`;
-}
+const calcularPerimetroTriangle = () => {
+  const medidas = document.querySelectorAll(".main__Triangle-form-input");
+  const ladoA = Number(medidas[0].value);
+  const ladoB = Number(medidas[1].value);
+  const base = Number(medidas[2].value);
+  if (!ladoA || !ladoB || !base) {
+    return (answerT.innerHTML = `Favor de ingresar un dato numericos🧐 en los campos necesarios.`);
+  }
+  return answerT.innerHTML =  perimetroT(ladoA, ladoB, base)
+};
+btnPerimetroT.addEventListener("click", calcularPerimetroTriangle);
 
 const calcularAreaCircle = () => {
-    const radio = document.querySelector(".main__Circle-form-input").value;
-    if(!radio){
-        return answer.innerHTML = `Favor de ingresar un dato numerico🧐`;
-    }
-    answer.innerHTML = `El <strong>area</strong> del circulo es de: <strong>${(Math.pow(radio,2) * Math.PI).toFixed(2)}cm<sup>2</sup></strong>`
-}
+  const radio = Number(document.querySelector(".main__Circle-form-input").value);
+  if (!radio) {
+    return (answerCi.innerHTML = `Favor de ingresar un dato numerico🧐`);
+  }
+  answerCi.innerHTML = `El <strong>area</strong> del circulo es de: <strong>${areaCi(radio).toFixed(2)}cm<sup>2</sup></strong>`;
+};
+btnAreaCi.addEventListener("click", calcularAreaCircle);
 
 const calcularPerimetroCircle = () => {
-    const radio = document.querySelector(".main__Circle-form-input").value;
-    if(!radio){
-        return answer.innerHTML = `Favor de ingresar un dato numerico🧐`;
-    }
-    answer.innerHTML = `El <strong>perimetro</strong> del circulo es de: <strong>${((2 * Math.PI) * radio).toFixed(2)}cm</strong>`
-}
+  const radio = document.querySelector(".main__Circle-form-input").value;
+  if (!radio) {
+    return (answerCi.innerHTML = `Favor de ingresar un dato numerico🧐`);
+  }
+  answerCi.innerHTML = `El <strong>perimetro</strong> del circulo es de: <strong>${perimetroCi(radio).toFixed(2)}cm</strong>`;
+};
+btnPerimetroCi.addEventListener("click", calcularPerimetroCircle);
